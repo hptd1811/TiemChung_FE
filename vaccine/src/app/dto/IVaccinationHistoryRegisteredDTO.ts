cca190523fc4b7256bf9d9ed8379d6888f4a133e
hptd1811 <tuandungdn123@gmail.com>
2023-01-05 21:40:42 +0700

    First commit

---
+++ vaccine/src/app/dto/IVaccinationHistoryRegisteredDTO.ts
@@ -0,0 +1,41 @@
+/**
+ * VaccinationHistoryRegistered : create by LongBP
+ */
+
+export interface IVaccinationHistoryRegisteredDTO {
+
+  vaccinationHistoryId : number;
+
+  patientId : number;
+
+  patientName : string;
+
+  patientDob : string;
+
+  patientGender : boolean;
+
+  patientGuardian : string;
+
+  patientPhone : string;
+
+  patientAddress : string;
+
+  vaccineName : string;
+
+  vaccineTypeName : string;
+
+  vaccinationHistoryStatus : boolean;
+
+  endTime : string;
+
+  dosage : string;
+
+  preStatus : string;
+
+  afterStatus : string;
+
+  vaccinationTimes : number;
+
+  vaccineQuantity: number;
+
+}
